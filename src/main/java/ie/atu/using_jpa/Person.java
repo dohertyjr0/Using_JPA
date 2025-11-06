a37d4d028d5ea665e62dad3df1e604d37a2b6b59
johnr <dohertyjr0@icloud.com>
2025-11-06 22:52:49 +0000

    initial commit

---
+++ src/main/java/ie/atu/using_jpa/Person.java
@@ -0,0 +1,23 @@
+package ie.atu.using_jpa;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "persons")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String employeeId;
+    private String position;
+    private String department;
+    private boolean admin;
+}
