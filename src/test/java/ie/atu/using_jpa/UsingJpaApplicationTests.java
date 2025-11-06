b4d2ffede5eb7e7751e5712c9d3cd62a59949064
johnr <dohertyjr0@icloud.com>
2025-11-06 22:33:45 +0000

    initial commit

---
+++ src/test/java/ie/atu/using_jpa/UsingJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.using_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsingJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
